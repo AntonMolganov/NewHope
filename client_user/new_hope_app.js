97a93f9716fd26e21dd70314f4cc38b60abc144b
VladimirPatrakov <51773606+VladimirPatrakov@users.noreply.github.com>
2019-06-16 14:05:49 +0500

    Add files via upload

--- client_user/new_hope_app.js
+++ client_user/new_hope_app.js
@@ -150,6 +150,8 @@ function init () {
 
 
     //функция определения реального времени, в которое отъезжает ближайший автобус от остановки
+    //time_dir = before, если необходимо найти автобус до переданного времени отправления
+    //time_dir = after, если надо найти ближайший автобус после переданного времени прихода на остановку
     /**в реальной программе будет обращение к апи расписания чтобы найти автобус
     * http://newhope/newhope/trips/nearest
     *    {
@@ -159,12 +161,13 @@ function init () {
     *    "stopName": "название остарновки"
     *    }
     */
-    const getBusTime = (time_leave_from_stop, segment) => {
+    const getBusTime = (time_leave_from_stop, segment, time_dir) => {
         function getRandomArbitrary(min, max) {
           return Math.random() * (max - min) + min;
         };
 
-        return time_leave_from_stop - getRandomArbitrary(60, 300);
+        var dir = (time_dir === 'before') ? -1 : 1;
+        return time_leave_from_stop + (dir * getRandomArbitrary(60, 300));
     };
 
 
@@ -205,7 +208,7 @@ function init () {
             const time_leave_from_stop = end_time_in_secs - ride_time;
 
             //время, в которое выезжает ближайший автобус раньше времени time_leave_from_stop по переданному маршруту
-            const real_time_leave_from_stop = getBusTime(time_leave_from_stop, segments.get(1));
+            const real_time_leave_from_stop = getBusTime(time_leave_from_stop, segments.get(1), 'before');
 
             var get_ready_time;
             if (time_ready == undefined) {
@@ -220,7 +223,17 @@ function init () {
             //время, в которое надо начать собраться - время выезда автобуса с остановки минус время пешком минус время сборов
             var start_ready_time = (real_time_leave_from_stop - walk_time - get_ready_time*60);
 
-            //var nowdate = Date();
+            var now_date = Date.parse(Date())/1000;
+
+            //предупреждение, если уже поздно
+            var warn = '';
+            if (start_ready_time < now_date) {
+
+                const arr_time_if_now = getBusTime(now_date + walk_time, segments.get(1), 'after') + ride_time;
+
+                warn = `<font color="red">Вам надо было выйти ${Math.round((now_date - start_ready_time)/60)} мин. назад. </br></br>
+                        Если вы выйдете сейчас, то будете в точке Б в ${secToTime(arr_time_if_now)}</font>`;
+            }
 
             //$('#result_list').html("");
             //$('#result_list').append(`Время в пути составит ${Math.round(ride_time/60)}мин. (${ride_time} сек). <br>Расстояние: ${length.text} (${Math.round(length.value)} м). <br>Время сбора: ${get_ready_time} мин. <br>Время, в которое надо прибыть: ${show_end_time}. <br>Время выхода: ${leave_time}`);
@@ -233,7 +246,8 @@ function init () {
                                         Время сбора: ${get_ready_time} мин. </br></br>
                                         Время, в которое надо прибыть: ${show_end_time}. </br></br>
                                         Время выхода: ${secToTime(leave_time)}. </br></br>
-                                        Время, в которое надо начать собираться: ${secToTime(start_ready_time)} </br></br>`);
+                                        Время, в которое надо начать собираться: ${secToTime(start_ready_time)} </br></br>
+                                        ${warn}`);
 
         };
     };
@@ -290,24 +304,7 @@ function init () {
     });
 
     const openMarkForm = (segments) => {
-        try {
-            var transportData = [];
-            for (var i = 0; i < segments.getLength(); i++) {
-                if (segments.get(i).properties._data.type === "walk") continue;
-                var segment = segments.get(i).properties._data.rawProperties.SegmentMetaData;
-                if (segment.Transports.length == 0) continue;
-                transportData.push({
-                    type: segment.Transports[0].type,
-                    name: segment.Transports[0].name
-                });
-            }
-            if (transportData.length === 0) alert("Вы не ездили на общественном транспорте и оценивать нечего");
-            //todo
-
-
-        }catch (err){
-            alert("Ошибка при формировании данных поездки, попробуйте в другой раз :(");
-        }
+        console.log('открываем форму оценки');
     };
 
     markButton.events.add(["click"], function (event) {
